becdfb56e09959b34c66f5e1cb18921031dde5cc
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-08-24 14:25:29 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+const CACHE_NAME = 'site-cache-v1';
+const CACHE_URLS = ['/', '/index.html', '/style.css', '/scriptstuf.js', '/icon.png', '/multiselect.js'];
 
-workbox.routing.registerRoute(
-  ({request}) => ['image', 'document', 'script', 'style'].includes(request.destination),
-  new workbox.strategies.CacheFirst({
-    cacheName: 'site-assets',
-    plugins: [new workbox.expiration.ExpirationPlugin({maxEntries: 100, maxAgeSeconds: 30 * 24 * 60 * 60})],
-  })
-);
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_URLS)));
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
