71c6dcd5a992de0c4f863e2371e3e82710ed6944
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-08-24 14:20:59 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+  ({request}) => ['image', 'document', 'script', 'style'].includes(request.destination),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'site-assets',
+    plugins: [new workbox.expiration.ExpirationPlugin({maxEntries: 100, maxAgeSeconds: 30 * 24 * 60 * 60})],
+  })
+);
